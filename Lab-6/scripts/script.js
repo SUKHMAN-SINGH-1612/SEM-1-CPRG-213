d5e2cfa9c631b321cb86f2ad0777abfa77ce16cc
Sukhman Singh <sukhmansinghbhogal@gmail.com>
2024-03-21 22:33:41 -0600

    name added

--- Lab-6/scripts/script.js
+++ Lab-6/scripts/script.js
@@ -1,4 +1,4 @@
-/****************** YOUR NAME: 
+/****************** YOUR NAME: Sukhman Singh
 
 The instructions describe the missing logic that is needed; you will translate these into JavaScript in the places indicated.
 
